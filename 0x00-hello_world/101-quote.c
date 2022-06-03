3a42cd939ef50265bb7e05ca26ee034505b51234
yousful <yousoophlekan@gmail.com>
2022-06-02 18:51:55 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry Point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1(Error)
+ */
+
+
+int main(void)
+{
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+write(2, err, 58);
+return (1);
+}
